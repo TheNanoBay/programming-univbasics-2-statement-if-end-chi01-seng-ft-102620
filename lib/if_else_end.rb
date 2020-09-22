7ecaea9dcbba537d4d56d753d4faae39f6e0d955
Mehdi A Noorani <noorani.mehdi@outlook.com>
2020-09-22 03:13:34 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,7 +1,7 @@
 # Write your solution 
 
-current_time = time.now 
-current_time = current_time.to_i 
+current_time = Time.now 
+current_time = current_time.to_i
 
 if current_time % 2 == 0 
   puts "Even!"
